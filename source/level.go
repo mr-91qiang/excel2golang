6c19574714b9c42648b5211530c862cf40979f19
page <48363274+mr-91qiang@users.noreply.github.com>
2025-01-21 17:56:38 +0800

    test

--- source/level.go
+++ source/level.go
@@ -1,11 +1,15 @@
 package source
 
 type Level struct {
-	GameLevelId int    `json:"game_level_id" xlsx:"关卡IVd" index:"1" `
-	Sub         string `json:"sub" xlsx:"关卡序号" index:"2" `
-	Cn          string `json:"cn" xlsx:"关卡名称（中文）" index:"3" `
-	En          string `json:"en" xlsx:"关卡名称（H5）" index:"4" `
-	Ru          string `json:"ru" xlsx:"RU" index:"5" `
-	De          string `json:"de" xlsx:"DE" index:"6" `
-	Fr          string `json:"fr" xlsx:"FR" index:"7" `
+	GameLevelId int    `json:"game_level_id" xlsx:"关卡ID" index:"1" `
+	CNSub       string `json:"CNSub" xlsx:"CN副关卡" index:"2" ` //
+	ENSub       string `json:"ENSub" xlsx:"EN副关卡" index:"3" ` //
+	RUSub       string `json:"RUSub" xlsx:"RU副关卡" index:"4" ` //
+	DESub       string `json:"DESub" xlsx:"DE副关卡" index:"5" ` //
+	FRSub       string `json:"FRSub" xlsx:"FR副关卡" index:"6" ` //
+	CnMain      string `json:"cnMain" xlsx:"CN主关卡" index:"7" `
+	EnMain      string `json:"enMain" xlsx:"EN主关卡" index:"8" `
+	RuMain      string `json:"ruMain" xlsx:"RU主关卡" index:"9" `
+	DeMain      string `json:"deMain" xlsx:"DE主关卡" index:"10" `
+	FrMain      string `json:"frMain" xlsx:"FR主关卡" index:"11" `
 }
